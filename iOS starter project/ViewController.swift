9009b026b46b89df2f1133d514fdb87a34978962
Davide Ceresola <davide.ceresola@satispay.com>
2021-04-03 15:23:04 +0200

    Initial Commit

---
+++ iOS starter project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS starter project
+//
+//  Created by Davide Ceresola on 03/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
